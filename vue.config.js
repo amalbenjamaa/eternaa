74910be4e5c6c6bcb92a48c770c818495351ec2d
amal <amalbenjamaa2@gmail.com>
2025-11-30 00:20:13 +0100

    Fix publicPath for eternaa repository

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,6 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/eterna/' // Remplacez 'eterna' par le nom de votre repository GitHub
+    ? '/eternaa/' // Nom du repository GitHub
     : '/',
 });
